0ec8ad39df579802c8827822489127964185b7e8
Ethan Farber <edf1247@gmail.com>
2024-07-17 18:29:43 -0400

    seperated the sorting algorithm into another class

--- bst.js
+++ bst.js
@@ -12,27 +12,48 @@ class Tree {
         this.root = null;
     }
 
-    buildTree(array){
-        return this.mergeSort(array);
+
+    
+    buildTree(array, start = 0, end = arr.length - 1){
+        let arr = array;
+        let mid = Math.ceil((start + end) / 2);
+        let root = new Node();
+        root.data = arr[mid];
+        if(start > end) {
+            return;
+        }
+        root.leftChild = this.buildTree(arr, start, mid - 1);
+        root.rightChild = this.buildTree(arr, mid + 1, end);
+        return root;
+    }
+
+    
+
+}
+
+
+class cleanData{
+    constructor(array){
+        this.array = array;
     }
 
     removeDuplicates(array){
         return [... new Set(array)];
     }
 
-    mergeSort(array) {
+    mergeSort(array = this.array) {
         let arr = this.removeDuplicates(array);
         if (arr.length < 2){
             return array;
         } 
-        var half = Math.floor(arr.length / 2);
-        var leftArr = arr.slice(0, half);
-        var rightArr = arr.slice(half);
+        let half = Math.floor(arr.length / 2);
+        let leftArr = arr.slice(0, half);
+        let rightArr = arr.slice(half);
         return this.merge(this.mergeSort(leftArr), this.mergeSort(rightArr));
     }
     
     merge(leftArr, rightArr) {
-        var sortedArr = [];
+        let sortedArr = [];
         while(leftArr.length && rightArr.length){
             if(leftArr[0] <= rightArr[0]){
                 sortedArr.push(leftArr.shift());
@@ -43,12 +64,10 @@ class Tree {
         }
         return [...sortedArr, ...leftArr, ...rightArr];
     }
-
-
+   
 }
 
+
 let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
-let tree = new Tree(arr);
-
-console.log(tree.buildTree(arr));
+let data = new cleanData(arr);
+let cleanData = data.mergeSort();
